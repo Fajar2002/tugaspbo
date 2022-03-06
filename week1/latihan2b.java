19b814bd22a8536b3ed2dd58a2151460c6564558
Fajar2002 <101049370+Fajar2002@users.noreply.github.com>
2022-03-06 22:11:27 +0700

    Add files via upload

---
+++ week1/latihan2b.java
@@ -0,0 +1,13 @@
+package week1;
+
+
+public class latihan2b {
+    public static void main(String[] args) {
+        
+        int a=6, t=8;
+        double luas;
+        
+        luas=0.5*a*t;
+        System.out.println("Luas Segitiga : "+luas);
+    }  
+}
